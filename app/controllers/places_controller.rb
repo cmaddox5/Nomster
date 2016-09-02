e3e929da314aeb012f72e872fe70bf108987460e
Christian Maddox <cmaddox5@vols.utk.edu>
2016-09-02 15:23:01 +0000

    Added functionality to edit current places.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @places = Place.all.paginate(:page => params[:page], :per_page => 5)
@@ -20,6 +20,12 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
   end
 
   private

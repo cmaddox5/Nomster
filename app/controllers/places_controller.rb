76fad0941909958f73d862ac9e656208769ad626
Christian Maddox <cmaddox5@vols.utk.edu>
2016-09-02 15:43:32 +0000

    Added user permissions for edit.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,10 +20,18 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+    
     @place.update_attributes(place_params)
     redirect_to root_path
   end

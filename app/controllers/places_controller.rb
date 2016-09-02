3ca3ee1649ea5c724544b9ddf2011e40b76a9668
Christian Maddox <cmaddox5@vols.utk.edu>
2016-09-02 15:06:19 +0000

    Added a show page to show details on a single place.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    #@places = Place.all
-    @places = Place.all.paginate(:page => params[:page], :per_page => 1)
+    @places = Place.all.paginate(:page => params[:page], :per_page => 5)
   end
 
   def new
@@ -13,6 +12,10 @@ class PlacesController < ApplicationController
   def create
     current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private
